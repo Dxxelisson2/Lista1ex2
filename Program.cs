da17e7989aabcd9abc9dc24109e0dca0a6128b1e
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-04-12 20:33:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v1, area;
+            Console.WriteLine("Digite o Valor da aresta");
+
+            v1 = double.Parse(Console.ReadLine());
+
+            area = Math.Pow(v1, 2);
+
+            Console.WriteLine("Area do quadrado:"+area);
+         
+        }
+    }
+}
